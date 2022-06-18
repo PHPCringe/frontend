e29b126922b283f14b152df8849a97bae63c5f90
zuramai <saugi.dev@gmail.com>
2022-06-18 22:22:47 +0700

    feat: collective card

---
+++ src/components/CollectiveCard.vue
@@ -0,0 +1,92 @@
+<script lang="ts" setup>
+
+const props = defineProps({
+    avatar: {
+        type: String,
+        default: "/images/avatar.png",
+    },
+    background: {
+        type: String,
+        default: "/background.png",
+    },
+    name: {
+        type: String,
+        default: "Github",
+    },
+    tags: {
+        type: Array,
+        default: ["Open Source", "Coding"],
+    },
+    bio: {
+        type: String,
+        default: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ",
+    },
+    totalDonation: {
+        type: Number,
+        default: 100000000,
+    },
+    targetDonation: {
+        type: Number,
+        default: 250000000,
+    },
+    currency: {
+        type: String,
+        default: "Rp"
+    },
+    contributors: {
+        type: Array,
+        default: [
+            {
+                name: "Andi",
+                avatar_url: "/images/avatar.png"
+            },
+            {
+                name: "Andi",
+                avatar_url: "/images/avatar.png"
+            },
+            {
+                name: "Andi",
+                avatar_url: "/images/avatar.png"
+            },
+        ]
+    }
+})
+
+const numberFormat = (num: number) => {
+    return new Intl.NumberFormat('id-ID',{style: 'currency', currency: 'IDR',maximumSignificantDigits: 1}).format(num)
+}
+</script>
+
+<template>
+<div class="card rounded-lg">
+    <div class="card-bg | relative bg-slate-700 h-20 xl:h-32 rounded-t-lg">
+        <img :src="avatar" :alt="`Image for ${name}`" class="avatar-img absolute w-16 h-16 p-1 bg-white rounded-full">
+    </div>
+    <div class="card-content | pt-8 px-5">
+        <h3 class="text-2xl font-semibold">Github</h3>
+
+        <!-- Tags -->
+        <ul class="flex gap-1 my-3">
+            <li v-for="tag in tags" >
+                <router-link :to="`/tags/${tag}`" class="tag p-1 px-2 bg-slate-200 rounded-full text-xs">{{tag}}</router-link>
+            </li>
+        </ul>
+
+        <!-- About -->
+        <h5 class="text-lg font-semibold text-gray-700 my-2">About</h5>
+        <p class="text-xs">{{bio}}</p>
+
+        <!-- Donation -->
+        <p>
+            <span>{{numberFormat(totalDonation)}}</span> 
+            <span></span>
+        </p>
+    </div>
+</div>
+</template>
+<style>
+.card .avatar-img {
+    bottom: -20px;
+    left: 15px;
+}
+</style>
