4467a16f5f5045d67588922393478d7a4293e10c
zuramai <saugi.dev@gmail.com>
2022-06-19 06:38:52 +0700

    feat: sign up

---
+++ src/pages/auth/register.vue
@@ -0,0 +1,77 @@
+
+<script setup lang="ts">
+import FormRadio from '~/components/forms/FormRadio.vue';
+const isNextStep = ref(true)
+const gotoNextStep = () => isNextStep.value = true
+</script>
+<template>
+<div flex items-center justify-center my-28>
+    <Tabs :tabs="{
+        'personal': 'Create Personal Account',
+        'organization': 'Create Organizational Account'
+        }">
+        <template #personal>
+            <div class="card-login | w-full lg:w-150 rounded-sm p-10 bg-white">
+                <h1 class="text-3xl mb-5 font-semibold text-black">Join Our Community</h1>
+                <p class="text-sm mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+
+                <form class="my-5">
+                    <form-input label="Display Name" placeholder="Input your display name here"></form-input>
+                    <form-input label="Email" placeholder=""></form-input>
+                    <form-input type="password" label="Password" placeholder=""></form-input>
+                    <form-input type="password" label="Confirm Password" placeholder=""></form-input>
+                    <form-button :block="true">Sign Up</form-button>
+                </form>
+                
+                <p class="text-center mt-10">Already have an account? <router-link to="/auth/sign-in" class="text-link text-link-gradient">Sign in</router-link></p>
+            </div>
+        </template>
+        <template #organization>
+            <div class="card-login | w-full lg:w-150 rounded-sm p-10 bg-white" >
+
+                <div v-if="!isNextStep">
+                    <h1 class="text-3xl mb-5 font-semibold text-black">Create an Organization</h1>
+                    <p class="text-sm mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+
+                    <form class="my-5">
+                        <form-input label="Display Name" placeholder="Input your display name here"></form-input>
+                        <form-input label="Email" placeholder=""></form-input>
+                        <form-input type="password" label="Password" placeholder=""></form-input>
+                        <form-input type="password" label="Confirm Password" placeholder=""></form-input>
+                        <form-button @click="gotoNextStep"  type="button" :block="true" >Next Step</form-button>
+                        
+                        <p class="text-center mt-10">Already have an account? <router-link to="/auth/sign-in" class="text-link text-link-gradient">Sign in</router-link></p>
+                    </form>
+                </div>
+                <div v-else>
+                    <h1 class="text-3xl mb-5 font-semibold text-black">Fill Out Your Organization Data</h1>
+                    <p class="text-sm mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+
+                    <form class="my-5">
+                        <div class="form-group | mb-5">
+                            <label for="orgtype" class="block font-bold text-black mb-3">Organization </label>
+                            <form-radio name="orgtype" id="profit" label="Profit" value="profit" class="mr-3"></form-radio>
+                            <form-radio name="orgtype" id="nonprofit" label="Non-profit" value="nonprofit" class="mr-3"></form-radio>
+                        </div>
+                        <form-textarea label="Bio" :rows="3"/>
+                        <form-input type="number" step="1000" label="Donation Goal" placeholder="Rp100.000" prefix="Rp"></form-input>
+                        <form-input label="Website" placeholder="yourwebsite.com"></form-input>
+                        <form-input label="Twitter" placeholder="@"></form-input>
+                        <form-textarea label="Description" :rows="6" placeholder="Description about your project"/>
+
+                        <form-button type="button" :block="true">Sign Up</form-button>
+                        
+                        <p class="text-center mt-10">Already have an account? <router-link to="/auth/sign-in" class="text-link text-link-gradient">Sign in</router-link></p>
+                    </form>
+                </div>
+            </div>
+        </template>
+    </Tabs>
+    
+</div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
